67ce91e5aa0b5cdd5e857e5b8b72a597855642ee
Yaseen Aiman Mohammed <34039174+yaseenaiman@users.noreply.github.com>
2025-01-11 20:33:14 +0300

    Delete SatGeom_Example_starlink directory

--- SatGeom_Example_starlink/nicefftnum.m
+++
@@ -1,9 +0,0 @@
-function nfft = nicefftnum(n)
-% Find a highly composite even number >= n
-%
-%  C. Wilson 10 October 1999 
-
-testvalues =[1,3,5,9,15,25,27];
-nt=2.^max(1,ceil(log2(n./testvalues))).*testvalues;
-nfft=min(nt);
-return
